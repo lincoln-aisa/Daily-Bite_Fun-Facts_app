c4bd7900588d47713bb5d06fc485e24488d508ec
Lincoln A <32209569+lincoln-aisa@users.noreply.github.com>
2025-08-23 16:43:03 +0200

    Update index.ts
    cleaned up

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,6 +1,4 @@
-console.log("ENTRY: index.ts executing");
 import { registerRootComponent } from "expo";
 import App from "./App";
 
-console.log("ENTRY: registerRootComponent being called");
 registerRootComponent(App);
